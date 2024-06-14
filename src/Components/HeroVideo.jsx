f93d31b87c3274811c160bc06f2b8448e6ea6564
Cora <csburgosrodriguez@student.fullsail.edu>
2024-06-14 00:47:50 -0400

    mobile hero video fix

--- src/Components/HeroVideo.jsx
+++ src/Components/HeroVideo.jsx
@@ -22,12 +22,18 @@ const HeroVideo = () => {
       }
     }
   }, []);
-  
+
   return (
     <div className="noselect w-fit h-fit text-grayscale-50 absolute z-10 flex flex-col justify-center items-center rounded-[50%]" id="repulse-div">
       <LazyMotion features={domAnimation} strict>
         <div className="relative w-screen h-screen overflow-hidden">
-          <m.video autoPlay muted loop className="absolute top-0 left-0 w-screen h-screen object-cover">
+          <m.video 
+          ref={videoRef}
+          autoPlay 
+          muted 
+          loop 
+          playsInline
+          className="absolute top-0 left-0 w-screen h-screen object-cover">
             <m.source src={heroVideo} type="video/mp4" />
           </m.video>
         </div>
