bec979a674179171b89c9211deb90382c6ff2dcf
Cora <csburgosrodriguez@student.fullsail.edu>
2024-06-13 20:57:12 -0400

    fixes to hero bg

--- src/Components/HeroVideo.jsx
+++ src/Components/HeroVideo.jsx
@@ -1,4 +1,5 @@
 import { m, LazyMotion, domAnimation } from "framer-motion";
+import { heroVideo } from "../assets";
 
 const HeroVideo = () => {
   return (
@@ -6,7 +7,7 @@ const HeroVideo = () => {
       <LazyMotion features={domAnimation} strict>
         <div className="relative w-screen h-screen overflow-hidden">
           <m.video autoPlay muted loop className="absolute top-0 left-0 w-screen h-screen object-cover">
-            <m.source src="/src/assets/video/【AMV】HatsuneMiku_FASHION4K60FPS」.mp4" />
+            <m.source src={heroVideo} type="video/mp4" />
           </m.video>
         </div>
       </LazyMotion>
